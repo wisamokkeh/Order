43f94953d6d40c4e6afa8b216b30ac3b4c685433
wisamokkeh <wisamokkeh2@gmail.com>
2018-11-14 06:04:55 +0200

    Show Details of Food

--- app/src/main/java/com/orders/wisam/order/FoodList.java
+++ app/src/main/java/com/orders/wisam/order/FoodList.java
@@ -1,5 +1,6 @@
 package com.orders.wisam.order;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -75,7 +76,10 @@ public class FoodList extends AppCompatActivity {
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongerClick) {
-                        Toast.makeText(FoodList.this, ""+local.getName(), Toast.LENGTH_SHORT).show();
+                        //start new activity
+                        Intent foodDetail = new Intent(FoodList.this,FoodDetail.class);
+                        foodDetail.putExtra("FoodId",adapter.getRef(position).getKey()); // send food id to new activity
+                        startActivity(foodDetail);
                     }
                 });
 
