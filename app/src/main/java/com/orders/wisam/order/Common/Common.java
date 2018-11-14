e87ffdf7790c7e158e853ab17c42e03c754a3f62
wisamokkeh <wisamokkeh2@gmail.com>
2018-11-14 03:23:02 +0200

    Initial commit

---
+++ app/src/main/java/com/orders/wisam/order/Common/Common.java
@@ -0,0 +1,7 @@
+package com.orders.wisam.order.Common;
+
+import com.orders.wisam.order.Model.User;
+
+public class Common {
+    public static User currentUser;
+}
---
+++ app/src/main/java/com/orders/wisam/order/SignUp.java
@@ -0,0 +1,82 @@
+package com.orders.wisam.order;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.orders.wisam.order.Model.User;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class SignUp extends AppCompatActivity {
+
+
+    MaterialEditText edtPhone, edtName, edtPassword;
+    Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        edtName = (MaterialEditText)findViewById(R.id.edtName);
+        edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+
+        //Firebase
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
+                mDialog.setMessage("Please Wait...");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //check if alrady exist
+                        if (dataSnapshot.child(edtPhone.getText().toString()).exists())
+                        {
+                            mDialog.dismiss();
+                            Toast.makeText(SignUp.this, "phone number alrady exist", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                        else
+                        {
+                            mDialog.dismiss();
+                            User user = new User(edtName.getText().toString(), edtPassword.getText().toString());
+                            table_user.child(edtPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUp.this, "Sign Up Successfuly", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+    }
+}
